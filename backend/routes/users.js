e8c84d713e00c6d30d946b9f7a42417a7cc83f22
Alan Chang <alan@alanchang.xyz>
2022-04-16 16:44:00 -0700

    Add signup/signin route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,6 +4,23 @@ const User = require('../models/user.model');
 const userRouter = express();
 userRouter.use(express.json());
 
+userRouter.get('/', async (req, res) => {
+  try {
+    User.find({}, (err, users) => {
+      if (err) {
+        // console.log(err);
+        res.status(400).json({ error: 'Error fetching users' });
+      } else {
+        // console.log('Users: ');
+        // console.log(users);
+        res.json(users);
+      }
+    });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
 userRouter.get('/:email', async (req, res) => {
   try {
     const { email } = req.params;
@@ -31,6 +48,25 @@ userRouter.post('/signup', async (req, res) => {
     });
   } catch (err) {
     res.status(500).send(err.message);
+  }
+});
+
+userRouter.post('/signin', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    // console.log(email, password);
+    User.find({ email }, (err, users) => {
+      if (err) {
+        // console.log(err);
+        res.status(400).json({ error: 'Error fetching users' });
+      } else if (users.length > 0 && users[0].password === password) {
+        res.status(200).send(users[0]);
+      } else {
+        res.status(400).send('Invalid password');
+      }
+    });
+  } catch (error) {
+    res.status(500).send(error);
   }
 });
 
