0c4b0af81e92b99b334697641ee73a94215c0be6
Zubair Sidhu <sidhuzubair@gmail.com>
2022-04-16 15:27:33 -0700

    Add chakra, react router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,24 @@
 import React from "react";
-import logo from "./logo.svg";
+import { ChakraProvider, Box } from "@chakra-ui/react";
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
+
+// Pages
+import Test from "./pages/TestPage/Test";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Router>
+        <Box className="page-container">
+          <Box className="content-wrap">
+            <Routes>
+              <Route path="/" element={<Test />} />
+            </Routes>
+          </Box>
+        </Box>
+      </Router>
+    </ChakraProvider>
   );
 }
 
---
+++ frontend/src/pages/TestPage/Test.jsx
@@ -0,0 +1,27 @@
+import { React } from "react";
+
+import styles from "./Test.module.css";
+import logo from "../../logo.svg";
+
+const Test = () => {
+  return (
+    <div className={styles.App}>
+      <header className={styles["App-header"]}>
+        <img src={logo} className={styles["App-logo"]} alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default Test;
